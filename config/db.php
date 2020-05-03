0a0f9eace1d7cd77c2a0c927d1c8d4c2eb977195
fauzigalihas <fauzigalihas@gmail.com>
2020-05-03 20:27:09 +0700

    gitignore

--- config/db.php
+++ config/db.php
@@ -7,8 +7,4 @@ return [
     'password' => '1234',
     'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
