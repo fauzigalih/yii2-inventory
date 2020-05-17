9f354aa98c99aee50ae66e0d0275e3221c9fa9e5
fauzigalihas <fauzigalihas@gmail.com>
2020-05-17 22:31:32 +0700

    update transaction model

--- models/Transaction.php
+++ models/Transaction.php
@@ -5,6 +5,7 @@ namespace app\models;
 use Yii;
 use yii\db\ActiveRecord;
 use yii\data\ActiveDataProvider;
+use yii\helpers\ArrayHelper;
 
 /**
  * This is the model class for table "transaction".
@@ -20,6 +21,10 @@ use yii\data\ActiveDataProvider;
  */
 class Transaction extends ActiveRecord
 {
+    public $fullName;
+    public $fromDate;
+    public $toDate;
+    public $active;
     /**
      * {@inheritdoc}
      */
@@ -36,7 +41,8 @@ class Transaction extends ActiveRecord
         return [
             [['userId', 'stockFirst', 'qtyTrx', 'stockFinal'], 'integer'],
             [['invoice', 'codeTrx'], 'string', 'max' => 50],
-            [['datePublished'], 'safe'],
+            [['datePublished', 'fullName', 'fromDate', 'toDate', 'active'], 'safe'],
+            [['datePublished'], 'default', 'value' => date('Y-m-d')],
         ];
     }
 
@@ -68,19 +74,67 @@ class Transaction extends ActiveRecord
         return $this->hasOne(ProductIn::className(), ['invoice' => 'codeTrx']);
     }
     
+    public function getProductOut(){
+        return $this->hasOne(ProductOut::className(), ['invoice' => 'codeTrx']);
+    }
+    
     public function search() {
+        $fromDate = $this->fromDate;
+        $toDate = $this->toDate;
+        if($fromDate == '') {
+            $fromDate = '';
+            $toDate = '';
+        }else if($toDate == ''){
+            $toDate = $this->fromDate;
+        }
         $query = self::find()
-            ->andFilterWhere(['like', 'invoice', $this->invoice])
-            ->andFilterWhere(['like', 'codeTrx', $this->codeTrx])
-            ->andFilterWhere(['like', 'userId', $this->userId]);
-
+            ->joinWith('user')
+            ->joinWith('productIn.products AS a')
+            ->joinWith('productOut.products AS b')
+            ->andFilterWhere(['like', 'transaction.invoice', $this->invoice])
+            ->andFilterWhere(['like', 'user.fullName', $this->fullName])
+            ->andFilterWhere(['like', 'transaction.codeTrx', $this->codeTrx])
+            ->andFilterWhere(['=', 'transaction.stockFirst', $this->stockFirst])
+            ->andFilterWhere(['=', 'transaction.qtyTrx', $this->qtyTrx])
+            ->andFilterWhere(['=', 'transaction.stockFinal', $this->stockFinal])
+            ->andFilterWhere(['between', 'transaction.datePublished', $fromDate, $toDate])
+            ->andFilterWhere(['or', ['a.active' => $this->active], ['b.active' => $this->active]]);
+        
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
             'pagination' => [
                 'pageSize' => 8
+            ],
+            'sort' => [
+                'attributes' => [
+                    'invoice' => 'invoice',
+                    'user.fullName' => 'user.fullName',
+                    'codeTrx' => 'codeTrx',
+                    'imageProduct' => 'imageProduct',
+                    'stockFirst' => 'stockFirst',
+                    'qtyTrx' => 'qtyTrx',
+                    'stockFinal' => 'stockFinal',
+                    'datePublished' => 'datePublished',
+                    'products.active' => 'products.active'
+                ]
             ]
         ]);
 
         return $dataProvider;
     }
+    
+    public static function getInvoiceData() {
+        $query = self::find()->max('invoice');
+        $noInvoice = (int) substr($query, 3, 3);
+        $noInvoice++;
+        $charInvoice = "TRX";
+        $newInvoice = $charInvoice . sprintf("%03s", $noInvoice);
+
+        return $newInvoice;
+    }
+    
+    public static function getListInvoice() {
+        return ArrayHelper::map(self::find()->all(), 'id', 'invoice');
+    }
+    
 }
